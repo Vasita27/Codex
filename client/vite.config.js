c24a316069ce36d438e4154bed695dba89a000a8
premchandkodali <168194824+premchandkodali@users.noreply.github.com>
2025-06-25 04:28:56 +0530

    Update vite.config.js prod

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,20 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-    port: 3000,
-    proxy: {
-      '/api/auth': {
-        target: 'http://server:5000',  // Express server in Docker
-        changeOrigin: true,
-      },
-      '/api': {
-        target: 'http://flask-ai:5001', // Flask server in Docker
-        changeOrigin: true,
-      }
-    }
-  }
-})
+export default defineConfig(({ mode }) => {
+  const isDev = mode === 'development';
+
+  return {
+    plugins: [react()],
+    server: isDev
+      ? {
+          host: true,
+          port: 3000,
+          proxy: {
+            '/api/auth': {
+              target: 'http://server:5000',
+              changeOrigin: true,
+            },
+            '/api': {
+              target: 'http://flask-ai:5001',
+              changeOrigin: true,
+            },
+          },
+        }
+      : undefined,
+    build: {
+      outDir: 'dist',
+    },
+  };
+});

b48395d4b466afa5ca1f87953963ea9b52dfafd2
premchandkodali <168194824+premchandkodali@users.noreply.github.com>
2025-06-25 06:36:26 +0530

    Update Login.jsx api chnage

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -13,7 +13,7 @@ function Login() {
 
   const handleLogin = async () => {
     try {
-      const res = await axios.post(`${serverUrl}/api/auth/login`, { username, password })
+      const res = await axios.post(`${serverUrl}/login`, { username, password })
       localStorage.setItem('token', res.data.token)
       navigate('/mainpage')
     } catch (err) {
